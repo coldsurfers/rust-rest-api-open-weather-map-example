efb146786b7dab74f99988cc72b00464a04386f0
yungblud <killi8n@gmail.com>
2024-03-09 15:58:23 +0900

    feat: implemented models

---
+++ src/models.rs
@@ -0,0 +1,17 @@
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct WeatherData {
+    main: Main,
+    weather: Vec<Weather>
+}
+
+#[derive(Deserialize)]
+struct Main {
+    temp: f64
+}
+
+#[derive(Deserialize)]
+struct Weather {
+    description: String
+}
